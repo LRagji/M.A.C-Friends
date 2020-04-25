d17408c7952fc6311d02e3ff0e34fdd8afef6988
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-04-25 16:36:26 +0530

    Added rest module to command line

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const commandModuleMap = new Map();
 
 commandModuleMap.set("redis-channel", "channels/redis-channel");
 commandModuleMap.set("simple-channel", "channels/simple-channel");
+commandModuleMap.set("rest-module", "modules/rest-module")
 
 let componentName = process.argv[2];
 if (componentName !== undefined) {
